8fcc75a4665d1ca08406c8bf646333b015045961
andreorto98 <andrea.ortone98@gmail.com>
2020-12-26 12:42:48 +0100

    corrected a bug in test_utilities

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -3,6 +3,7 @@
 
 import unittest
 import cv2
+import os
 
 from scopa.utilities import get_cards
 #export PYTHONPATH=/Users/andrea/Desktop/computing_methods/scopa
@@ -10,10 +11,10 @@ from scopa.utilities import get_cards
 
 class TestCore(unittest.TestCase):
     def test_get_cards(self):
-        img0 = cv2.imread('test_img/get_cards0.jpg',cv2.IMREAD_COLOR)
+        img0 = cv2.imread('tests/test_img/get_cards0.jpg',cv2.IMREAD_COLOR)
         lenght = len(get_cards(img0, 50))
         self.assertEqual(lenght, 0, f'get_cards test failed: founded {lenght}, instead of 0')
-        img3 = cv2.imread('test_img/get_cards3.jpg',cv2.IMREAD_COLOR)
+        img3 = cv2.imread('tests/test_img/get_cards3.jpg',cv2.IMREAD_COLOR)
         lenght = len(get_cards(img3, 50))
         self.assertEqual(lenght, 3, f'get_cards test failed: founded {lenght}, instead of 3')
 
